5dcec3e72c9be73945727b7c70c99139418926ca
oo-infty <oo-infty@outlook.com>
2024-01-14 23:42:25 +0800

    feat: update font settings

--- modules/fonts.nix
+++ modules/fonts.nix
@@ -15,7 +15,7 @@
     fontconfig.defaultFonts = {
       serif = [ "Noto Serif" "Noto Color Emoji" ];
       sansSerif = [ "Noto Sans" "Noto Color Emoji" ];
-      monospace = [ "SourceCodePro Nerd Font" "Noto Color Emoji" ];
+      monospace = [ "Source Code Pro" "Noto Color Emoji" ];
       emoji = [ "Noto Color Emoji" ];
     };
   };
