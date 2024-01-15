72d8a384a45a662edd3ace91a369f365d48e1f4d
oo-infty <oo-infty@outlook.com>
2024-01-15 20:06:47 +0800

    feat: enable automatic garbage collection

--- system/oo-laptop/default.nix
+++ system/oo-laptop/default.nix
@@ -11,10 +11,17 @@
     ./hardware-configuration.nix
   ];
 
+  # Nix settings
   nix.settings.experimental-features = [
     "nix-command"
     "flakes"
   ];
+
+  nix.gc = {
+    automatic = true;
+    dates = "daily";
+    options = "--delete-older-than 7d";
+  };
 
   # GRUB 2
   boot.loader = {
