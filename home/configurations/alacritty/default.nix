44a959f7999f6365f87a9cae4bbf61f5527a616b
oo-infty <oo-infty@outlook.com>
2024-01-25 18:17:26 +0800

    feat(alacritty): add themes for alacritty

--- home/configurations/alacritty/default.nix
+++ home/configurations/alacritty/default.nix
@@ -1,5 +1,17 @@
-{ config, pkgs, ... }:
+{ config, pkgs, flags, ... }:
 
-{
-  home.file.".config/alacritty/alacritty.toml".source = ./alacritty.toml;
+let
+  colorscheme = if flags.ui.colorscheme == "Tokyo Night Storm" then "tokyo-night-storm"
+    else if flags.ui.colorscheme == "One Dark" then "one-dark"
+    else builtins.abort "Invalid value `${flags.ui.colorscheme}` for `flags.ui.colorscheme`";
+in {
+  home.file.".config/alacritty/alacritty.toml".source = pkgs.substitute {
+    src = ./alacritty.toml;
+    replacements = [ "--replace @%colorscheme-placeholder%@ ${colorscheme}" ];
+  };
+
+  home.file.".config/alacritty/themes" = {
+    source = ./themes;
+    recursive = true;
+  };
 }
