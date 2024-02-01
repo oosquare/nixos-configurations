293cadd54b4d38699b45e3b5f7bc6ada2ab075ee
oo-infty <oo-infty@outlook.com>
2024-02-01 11:56:00 +0800

    feat(ncmpcpp): add music visualizer support

--- home/configurations/mpd/default.nix
+++ home/configurations/mpd/default.nix
@@ -7,9 +7,17 @@
 
     extraConfig = ''
       auto_update "yes"
+
       audio_output {
         type "pipewire"
         name "MPD PipeWire Output"
+      }
+
+      audio_output {
+        type "fifo"
+        name "MPD Visualizer Data FIFO"
+        path "/tmp/mpd-visualizer-data.fifo"
+        format "44100:16:2"
       }
     '';
   };
--- home/configurations/ncmpcpp/default.nix
+++ home/configurations/ncmpcpp/default.nix
@@ -1,5 +1,19 @@
 { config, lib, pkgs, ... }:
 
 {
-  programs.ncmpcpp.enable = true;
+  programs.ncmpcpp = {
+    enable = true;
+
+    package = pkgs.ncmpcpp.override {
+      visualizerSupport = true;
+    };
+
+    settings = {
+      visualizer_output_name = "MPD Visualizer Data FIFO";
+      visualizer_data_source = "/tmp/mpd-visualizer-data.fifo";
+      visualizer_in_stereo = "yes";
+      visualizer_type = "spectrum";
+      visualizer_look = "+|";
+    };
+  };
 }
