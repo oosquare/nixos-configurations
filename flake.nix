d56479cfbb93de8fef87e9937b6fe4ab1705fc2c
oo-infty <oo-infty@outlook.com>
2024-02-03 23:01:51 +0800

    chore: update system

--- flake.nix
+++ flake.nix
@@ -4,8 +4,8 @@
   nixConfig = {
     substituters = [
       # Mirror sites of the official cache server
-      "https://mirror.sjtu.edu.cn/nix-channels/store"
       "https://mirrors.ustc.edu.cn/nix-channels/store"
+      "https://mirror.sjtu.edu.cn/nix-channels/store"
       "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
 
       # NixOS's official cache server
