55455a29aa483a7a38df365a8f389d4bc311b21d
oo-infty <oo-infty@outlook.com>
2024-02-07 20:46:31 +0800

    refactor(helix): rewrite helix configurations with Nix

---
+++ home/configurations/helix/themes/one-dark-transparent.nix
@@ -0,0 +1,20 @@
+{ config, lib, pkgs, ... }:
+
+{
+  inherits = "onedark";
+
+  "ui.background" = { fg = "foreground"; };
+  "ui.cursorline.primary" = { bg = "cursorline"; };
+  "ui.cursor.match" = { fg = "foreground"; bg = "light-gray"; };
+  "ui.menu" = { fg = "brighter-gray"; };
+  "ui.menu.selected" = { fg = "brighter-gray"; bg = "light-gray"; };
+  "ui.help" = { fg = "foreground"; };
+  "ui.popup" = { fg = "brighter-gray"; };
+  "ui.statusline" = { fg = "foreground"; };
+  "ui.statusline.inactive" = { fg = "light-gray"; };
+  "ui.window" = { fg = "light-gray"; };
+  
+  palette = {
+    brighter-gray = "#8e98ab";
+  };
+}
