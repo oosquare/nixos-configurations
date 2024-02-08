dbc570b6d2634738089a0266b11bca1629db197e
oo-infty <oo-infty@outlook.com>
2024-02-08 23:25:45 +0800

    refactor(mako): rewrite mako configurations in Nix

--- home/configurations/mako/default.nix
+++ home/configurations/mako/default.nix
@@ -1,5 +1,40 @@
 { config, lib, pkgs, ... }:
 
 {
-  home.file.".config/mako/config".source = ./config;
+  services.mako = {
+    enable = true;
+
+    sort = "-time";
+    layer = "overlay";
+
+    width = 400;
+    height = 120;
+    margin = "12";
+    padding = "12,20";
+    borderSize = 2;
+    borderRadius = 12;
+
+    borderColor = "#99c0d0";
+    backgroundColor = "#2e34407f";
+
+    icons = true;
+    maxIconSize = 64;
+
+    defaultTimeout = 5000;
+    ignoreTimeout = true;
+
+    font = "sans-serif 12";
+
+    extraConfig = ''
+      [urgency=low]
+      border-color=#cccccc
+
+      [urgency=normal]
+      border-color=#99c0d0
+
+      [urgency=critical]
+      border-color=#bf616a
+      default-timeout=0
+    '';
+  };
 }
