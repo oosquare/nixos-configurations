2d9fd1fa2fc673ef684aa0ec1c3e476daf6463a7
oo-infty <oo-infty@outlook.com>
2024-02-15 11:28:27 +0800

    fix(hardware): NVIDIA prime offload can't work

--- system/oo-laptop/hardware.nix
+++ system/oo-laptop/hardware.nix
@@ -81,13 +81,24 @@
     prime = {
       offload = {
         enable = true;
-        enableOffloadCmd = true;
+        enableOffloadCmd = false;
       };
 
       intelBusId = "PCI:0:2:0";
       nvidiaBusId = "PCI:1:0:0";
     };
   };
+
+  environment.systemPackages = [
+    (pkgs.writeShellScriptBin "nvidia-offload" ''
+      export __NV_PRIME_RENDER_OFFLOAD=1
+      export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
+      export __GLX_VENDOR_LIBRARY_NAME=nvidia
+      export __VK_LAYER_NV_optimus=NVIDIA_only
+      unset __EGL_VENDOR_LIBRARY_FILENAMES
+      exec "$@"
+    '')
+  ];
 
   # Network
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
