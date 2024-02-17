41f6010bd731ccd4ec56f589963c25eb482c292b
oo-infty <oo-infty@outlook.com>
2024-02-17 21:16:02 +0800

    feat(zellij): add zellij configurations

--- home/configurations/default.nix
+++ home/configurations/default.nix
@@ -19,6 +19,7 @@
     ./tofi
     ./waybar
     ./xdg
+    ./zellij
     ./zsh
   ];
 }
---
+++ home/configurations/zellij/default.nix
@@ -0,0 +1,6 @@
+{ config, lib, pkgs, ... }:
+
+{
+  programs.zellij.enable = true;
+  home.file.".config/zellij/config.kdl".source = ./config.kdl;
+}
