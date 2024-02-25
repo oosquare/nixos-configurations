160f07688dc189e6acf67d2b019dde3377880629
oo-infty <oo-infty@outlook.com>
2024-02-25 09:32:03 +0800

    refactor(qt): enable qt only when there exists at least one desktop

--- home/configurations/qt/default.nix
+++ home/configurations/qt/default.nix
@@ -1,9 +1,17 @@
 { config, lib, pkgs, ... }:
 
-{
-  qt = {
-    enable = true;
-    platformTheme = "qtct";
-    style.name = "kvantum";
-  };
+let
+  flags = config.flags;
+in {
+  config = let
+    hasDesktop = builtins.any (entry: entry.enable == true) (with flags.desktop; [
+      gnome
+      hyprland
+    ]);
+  in
+    lib.mkIf hasDesktop {
+      qt.enable = true;
+      qt.platformTheme = "qtct";
+      qt.style.name = "kvantum";
+    };
 }
