2490037ddf9bfc788fa2d45a3ff14f8e8a1d1941
oo-infty <oo-infty@outlook.com>
2024-05-03 22:58:50 +0800

    fix(vscode, lx-music-desktop): blurry UI on GNOME

--- overlays/lx-music-desktop/default.nix
+++ overlays/lx-music-desktop/default.nix
@@ -2,6 +2,6 @@
 
 final: prev: {
   lx-music-desktop = prev.lx-music-desktop.override {
-    commandLineArgs = "--ozone-platform-hint=auto --enable-features=WaylandWindowDecorations --enable-wayland-ime";
+    commandLineArgs = "--ozone-platform-hint=auto --enable-features=WaylandWindowDecorations --disable-features=WaylandFractionalScaleV1 --enable-wayland-ime";
   };
 }
--- overlays/vscode/default.nix
+++ overlays/vscode/default.nix
@@ -2,6 +2,6 @@
 
 final: prev: {
   vscode = prev.vscode.override {
-    commandLineArgs = "--ozone-platform-hint=auto --enable-features=WaylandWindowDecorations --enable-wayland-ime";
+    commandLineArgs = "--ozone-platform-hint=auto --enable-features=WaylandWindowDecorations --disable-features=WaylandFractionalScaleV1 --enable-webrtc-pipewire-capturer --enable-wayland-ime --disable-gpu";
   };
 }
