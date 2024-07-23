41a46f67dbf3299c3c7f279f300e09461e41f228
oo-infty <oo-infty@outlook.com>
2024-07-23 20:51:03 +0800

    feat(plasma): uninstall unwanted apps

--- modules/nixos/desktop/environment/plasma/default.nix
+++ modules/nixos/desktop/environment/plasma/default.nix
@@ -9,6 +9,13 @@ in {
     services.displayManager.sddm.enable = true;
     services.displayManager.sddm.wayland.enable = true;
     services.desktopManager.plasma6.enable = true;
+
+    environment.plasma6.excludePackages = with pkgs.kdePackages; [
+      kate
+      khelpcenter
+      konsole
+      kwrited
+    ];
   };
 }
 
