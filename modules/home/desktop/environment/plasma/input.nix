5bebffec1c0c8ee33669e970a5aebef2703cfc25
oo-infty <oo-infty@outlook.com>
2024-07-31 11:26:05 +0800

    feat(plasma): add input devices configurations

--- modules/home/desktop/environment/plasma/input.nix
+++ modules/home/desktop/environment/plasma/input.nix
@@ -4,11 +4,29 @@ let
   flags = config.flags.packages.desktop.environment.plasma;
 in {
   config = lib.mkIf flags.enable {
-    programs.plasma.configFile = {
-      kcminputrc.Keyboard.RepeatDelay = 250;
+    programs.plasma.input = {
+      keyboard = {
+        layouts = [ "us" ];
+        repeatDelay = 250;
+      };
 
-      kcminputrc."Libinput/9639/64104/CX Gaming Mouse".PointerAcceleration = 0.400;
-      kcminputrc."Libinput/9639/64104/CX Gaming Mouse".PointerAccelerationProfile = 1;
+      mice = lib.singleton {
+        enable = true;
+        name = "CX Gaming Mouse";
+        vendorId = "9639";
+        productId = "64104";
+        acceleration = 0.4;
+        accelerationProfile = "none";
+      };
+
+      touchpads = lib.singleton {
+        enable = true;
+        name = "ELAN07C6:00 04F3:32C7 Touchpad";
+        vendorId = "1267";
+        productId = "12999";
+        naturalScroll = true;
+        tapToClick = true;
+      };
     };
   };
 }
