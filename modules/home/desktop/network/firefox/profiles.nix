b635f97185bbc179eac12312468ea11027bfbdf6
oo-infty <oo-infty@outlook.com>
2024-08-22 23:20:29 +0800

    feat(firefox): add search engines

--- modules/home/desktop/network/firefox/profiles.nix
+++ modules/home/desktop/network/firefox/profiles.nix
@@ -8,9 +8,22 @@ in {
       id = 0;
       isDefault = true;
       search = {
-        default = "Bing";
+        default = "Google";
         force = true;
         engines = {
+          "GitHub" = {
+            urls = lib.singleton {
+              template = "https://github.com/search";
+              params = [
+                { name = "q"; value = "{searchTerms}"; }
+                { name = "type"; value = "repositories"; }
+              ];
+            };
+
+            icon = "github";
+            definedAliases = [ "@gh" ];
+          };
+
           "NixOS Packages" = {
             urls = lib.singleton {
               template = "https://search.nixos.org/packages";
@@ -36,6 +49,33 @@ in {
             icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
             definedAliases = [ "@no" ];
           };
+
+          "Home Manager Options" = {
+            urls = lib.singleton {
+              template = "https://home-manager-options.extranix.com";
+              params = [
+                { name = "query"; value = "{searchTerms}"; }
+                { name = "release"; value = "master"; }
+              ];
+            };
+
+            icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+            definedAliases = [ "@nh" ];
+          };
+
+          "NixOS Wiki" = {
+            urls = lib.singleton {
+              template = "https://wiki.nixos.org/w/index.php";
+              params = [
+                { name = "search"; value = "{searchTerms}"; }
+              ];
+            };
+
+            icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+            definedAliases = [ "@nw" ];
+          };
+
+          "Bing".metaData.hidden = true;
         };
       };
     };
