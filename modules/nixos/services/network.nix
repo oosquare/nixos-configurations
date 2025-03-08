2e764218bb8ba718abe7bfb1faecacdf596a8783
oosquare <oosquare@outlook.com>
2025-03-08 19:00:44 +0800

    chore: update system

--- modules/nixos/services/network.nix
+++ modules/nixos/services/network.nix
@@ -6,9 +6,6 @@ in {
   config = lib.mkIf flags.enable {
     networking.networkmanager.enable = true;
 
-    services.tailscale = {
-      enable = true;
-      authKeyFile = config.age.secrets."tailscale-auth.age".path;
-    };
+    services.tailscale.enable = true;
   };
 }
