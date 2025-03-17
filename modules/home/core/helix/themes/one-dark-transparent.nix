27da25c9f34acd0e6a5dfc52c06a0f918b640dda
oosquare <oosquare@outlook.com>
2025-03-17 18:24:44 +0800

    feat(helix): update themes

--- modules/home/core/helix/themes/one-dark-transparent.nix
+++ modules/home/core/helix/themes/one-dark-transparent.nix
@@ -8,13 +8,13 @@
   "ui.background" = { fg = "foreground"; };
   "ui.cursorline.primary" = { bg = "cursorline"; };
   "ui.cursor.match" = { fg = "foreground"; bg = "light-gray"; };
-  "ui.menu" = { fg = "brighter-gray"; };
-  "ui.menu.selected" = { fg = "brighter-gray"; bg = "light-gray"; };
+  "ui.menu" = { fg = "brighter-gray"; bg = "light-black"; };
+  "ui.menu.selected" = { fg = "white"; bg = "gray"; };
   "ui.help" = { fg = "foreground"; };
-  "ui.popup" = { fg = "brighter-gray"; };
+  "ui.popup" = { fg = "brighter-gray"; bg = "light-black"; };
   "ui.statusline" = { fg = "foreground"; };
   "ui.statusline.inactive" = { fg = "light-gray"; };
-  "ui.window" = { fg = "light-gray"; };
+  "ui.window" = { fg = "light-gray"; bg = "light-black"; };
   
   palette = {
     brighter-gray = "#8e98ab";
--- modules/home/core/helix/themes/tokyo-night-storm-transparent.nix
+++ modules/home/core/helix/themes/tokyo-night-storm-transparent.nix
@@ -6,13 +6,13 @@
   "ui.background" = { fg = "foreground"; };
   "ui.cursorline.primary" = { bg = "cursorline"; };
   "ui.cursor.match" = { fg = "foreground"; bg = "light-gray"; };
-  "ui.menu" = { fg = "brighter-gray"; };
-  "ui.menu.selected" = { fg = "brighter-gray"; bg = "light-gray"; };
+  "ui.menu" = { fg = "brighter-gray"; bg = "light-black"; };
+  "ui.menu.selected" = { fg = "white"; bg = "gray"; };
   "ui.help" = { fg = "foreground"; };
-  "ui.popup" = { fg = "brighter-gray"; };
+  "ui.popup" = { fg = "brighter-gray"; bg = "light-black"; };
   "ui.statusline" = { fg = "foreground"; };
   "ui.statusline.inactive" = { fg = "light-gray"; };
-  "ui.window" = { fg = "light-gray"; };
+  "ui.window" = { fg = "light-gray"; bg = "light-black"; };
 
   palette = {
     background_highlight = "#6c76a6";
